9f5f62c60cd1e67ee68dbe836f112c39ae015168
Chaooohs <01nikka01@gmail.com>
2023-07-13 21:45:45 +0300

    adsf

--- js/homework_js_6/js/style.js
+++ js/homework_js_6/js/style.js
@@ -93,3 +93,122 @@ function add(el) {
 }
 
 //================================================================
+
+// engine -------------------------------------------------------------
+class Engine {
+  constructor(manufacturer, power) {
+    this.manufacturer = manufacturer;
+    this.power = power;
+  }
+  get engineInfo() {
+    return `
+    company: ${this.manufacturer}
+    power: ${this.power}`;
+  }
+}
+
+// driver -------------------------------------------------------------
+class Driver {
+  constructor(pib, drivingExperience) {
+    this.pib = pib;
+    this.drivingExperience = drivingExperience;
+  }
+  get driverInfo() {
+    return `
+    pib: ${this.pib}
+    drivingExperience: ${this.drivingExperience}`;
+  }
+  set driverInfo(newPib) {
+    this.pib = newPib;
+  }
+}
+
+// car -------------------------------------------------------------------
+class Car {
+  constructor(brand, carClass, weight, driver, engine) {
+    this.brand = brand;
+    this.carClass = carClass;
+    this.weight = weight;
+    this.driver = driver;
+    this.engine = engine;
+  }
+  start(startPrint) {
+    document.write(`<h1>${startPrint}</h1>`);
+  }
+  stop(stopPrint) {
+    document.write(`<h1>${stopPrint}</h1>`);
+  }
+  turnRight(turnRightPrint) {
+    document.write(`<h1>${turnRightPrint}</h1>`);
+  }
+  turnLeft(turnLeftPrint) {
+    document.write(`<h1>${turnLeftPrint}</h1>`);
+  }
+  toString() {
+    return `
+    марка автомобіля: ${this.brand} 
+    клас автомобіля: ${this.carClass} 
+    вага автомобіля: ${this.weight}
+    водій: ${this.driver}
+    мотор: ${this.engine}`;
+  }
+}
+
+// lorry --------------------------------------------------------------
+class Lorry extends Car {
+  constructor(brand, carClass, weight, driver, engine, cargoCapacity) {
+    super(brand, carClass, weight, driver, engine);
+    this.cargoCapacity = cargoCapacity;
+  }
+  get lorryInfo() {
+    return `
+    cargoCapacity: ${this.cargoCapacity}`;
+  }
+}
+
+// SportCar -------------------------------------------------------------
+class SportCar extends Car {
+  constructor(brand, carClass, weight, driver, engine, maxSpeed) {
+    super(brand, carClass, weight, driver, engine);
+    this.maxSpeed = maxSpeed;
+  }
+  get sportCarInfo() {
+    return `
+    maxSpeed: ${this.maxSpeed}`;
+  }
+}
+
+// values ----------------------------------------------------------------
+const engineOne = new Engine("bmv", 300);
+const engineTwo = new Engine("mercedes", 400);
+
+const driverOne = new Driver("Ivan Petrov", 15);
+const driverTwo = new Driver("Ivan Sidorov", 20);
+driverOne.driverInfo = "Dave Murray";
+
+const carOne = new Car("bmv", "passenger", 700, driverOne.pib, engineOne.power);
+const carTwo = new Car("audi", "passenger", 500, driverTwo.pib, engineTwo.power);
+
+const lorry = new Lorry("bmv", "truck", 1500, driverOne.pib, engineOne.power, 1000);
+const sportCar = new SportCar("audi", "sportcar", 500, driverTwo.pib, engineTwo.power, 350);
+
+// console --------------------------------------------------------------------
+console.log(engineOne.engineInfo);
+console.log(engineTwo.engineInfo);
+
+console.log(driverOne.driverInfo);
+console.log(driverTwo.driverInfo);
+
+console.log(carOne.toString());
+console.log(carTwo.toString());
+
+console.log(lorry.lorryInfo);
+console.log(sportCar.sportCarInfo);
+
+// print ---------------------------------------------------------------------------
+// print(carOne.start("Поїхали"));
+// print(carOne.stop("Зупиняємося"));
+// print(carOne.turnRight("Поворот праворуч"));
+// print(carOne.turnLeft("Поворот ліворуч"));
+
+//========================================
